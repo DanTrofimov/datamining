55118e307c2c31ab66bfd9be846c6f5e5c645858
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-03-17 22:47:13 +0300

    fixes

---
+++ homework2/main.py
@@ -0,0 +1,128 @@
+from pprint import pprint
+import vk_api
+import re
+
+import psycopg2 as psy
+import pandas as pd
+import matplotlib.pyplot as plt
+import vk_api
+
+# remove symbols: " ) ( # & << » [ ] «
+# remove words with: digits / length==0or 1 club
+removingChars = ["(", ")", "[", "]", "«", "»", "&", "!", "\""]
+banningSubs = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "/", "club", "\\", ";", "#", "@"]
+
+
+def remove_char(input_word, char):
+    resultWord = input_word.replace(char, "")
+    return resultWord
+
+
+def clear_word(word_clear):  # Убираем ненужные символы
+    for char in removingChars:
+        word_clear = remove_char(word_clear, char)
+    return word_clear
+
+
+def filter_word(filtering_word):  # Удаляем неподходящие слова
+    if len(filtering_word) == 1 or len(filtering_word) == 0:
+        return False
+    for sub in banningSubs:
+        if sub in filtering_word:
+            return False
+    return True
+
+
+def get_words(text):  # Рaзбиваем на слова данные
+    text = text.replace("\n", " ")
+    text = text.replace(",", "").replace(".", "").replace("?", "").replace("!", "")
+    text = text.lower()
+    words = text.split()
+    words.sort()
+    return words
+
+
+def get_words_dict(words):  # Подсчет слов
+    words_filtered = []
+    clear_words = []
+    words_dictionary = dict()
+
+    for word_item in words:
+        if filter_word(word_item):
+            words_filtered.append(word_item)
+
+    for word_item in words_filtered:
+        clear_words.append(clear_word(word_item))
+
+    for word_item in clear_words:
+        if word_item in words_dictionary:
+            words_dictionary[word_item] = words_dictionary[word_item] + 1
+        else:
+            words_dictionary[word_item] = 1
+    return words_dictionary
+
+
+vk_session = vk_api.VkApi("+79053749485", "ila09202000")  # Проводим авторизацию(логин и пароль от вк)
+vk_session.auth()
+id = 'id641678009'  # Вводим свой айдишник
+api = vk_session.get_api()
+
+groups = api.groups.get()
+pprint(groups)
+
+walls = api.wall.get(domain='itis_kfu', count=200)  # Получили все посты
+
+resultText = ""
+
+# fixme: Добавление текстовых полей, если возможно, то добавить еще
+for item in walls['items']:
+    resultText = resultText + item['text']
+    if ('title' in item):
+        resultText += item['title']
+
+words_dict = get_words_dict(get_words(resultText))  # Итоговый словарь (слово - кол-во вхождений)
+
+words_dict = dict(sorted(words_dict.items(), key=lambda dict_item: dict_item[1]))
+
+# df = pd.DataFrame.from_dict(words_dict, orient='index').transpose()
+
+plt.title('Words statistics')
+
+words_top = 20
+
+bars = list(words_dict.values())[len(words_dict)-words_top:]
+xticks = list(words_dict.keys())[len(words_dict)-words_top:]
+
+plt.bar(range(words_top), bars, align='center')
+plt.xticks(range(words_top), xticks, rotation='vertical')
+plt.tight_layout()
+
+plt.savefig('words-top.png')
+
+plt.show()
+
+# logs
+for word in words_dict:
+    print(len(word), word.ljust(20), words_dict[word])
+
+# database insert
+conn = psy.connect(
+    dbname='manager',
+    user='postgres',
+    password='admin',
+    host='localhost',
+    port=5432,
+)
+cur = conn.cursor()
+cur.execute("""SELECT datname from pg_database""")
+
+values = ',\n'.join(["""('{0}', {1})""".format(
+    word,
+    counter,
+) for word, counter in words_dict.items()])
+
+cur.execute("""INSERT INTO words (word, count) VALUES 
+{0}""".format(values))
+conn.commit()
+cur.close()
+conn.close()
---
+++ homework2/new/collect_dag.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+
+
+default_args = {
+'owner': 'admin',
+'depends_on_past': False,
+'email': ['airflow@example.com'],
+'email_on_failure': False,
+'email_on_retry': False,
+'retries': 1,
+'retry_delay': timedelta(minutes=5),
+
+}
+dag = DAG(
+'collect_dag',
+default_args=default_args,
+description='A simple tutorial DAG',
+schedule_interval=None,
+start_date=days_ago(2),
+tags=['vk_api'],
+)
+
+
+BashOperator(
+task_id='collect_task',
+bash_command='python3 /opt/datamining/homework #2/new/DataPiplineTask.py 200 itis_kfu database-2.c5y5s391niz5.us-east-1.rds.amazonaws.com postgres qwerty007 postgres 5432',
+dag=dag)
